a65dfd01835091c6ecccc8808e61290bca9db2ed
Bishara Korkor <Bishara.Korkor@gmail.com>
2015-11-10 14:50:28 -0800

    added filelackey.py

---
+++ filelackey.py
@@ -0,0 +1,22 @@
+from datetime import datetime as dt
+
+
+class BossFile:
+    def __init__(self, filename, mode, script, purpose):
+        self.filename = filename
+        self.script = script
+        self.f = open(filename, mode)
+        self.purpose = purpose
+
+    def write(self, string):
+        self.f.write(string)
+
+    def close(self):
+        self.f.close()
+        lackey = open(self.filename + ".lackey", 'w')
+        date = dt.now()
+        lackey.write(self.filename + '\n')
+        lackey.write('Generation Date: ' + str(date.month) + '/' + str(date.day) + '/' + str(date.year) + ' at ' +
+                     str(date.hour) + ':' + str(date.minute) + '\n')
+        lackey.write('Generator Module: ' + self.script + '\n')
+        lackey.write('File Purpose: ' + self.purpose)
